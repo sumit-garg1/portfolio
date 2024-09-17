6de7812519e089d5337e553ad1f5ce29ff160cdc
sumit-garg1 <sumitgarg2fbd@gmail.com>
2024-09-17 15:03:41 +0530

    changes in some file

--- sumit/urls.py
+++ sumit/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.home,name="home"),
+    path("about/",views.about,name="about"),
     path("about",views.about,name="about"),
-    path("skills",views.skills,name="skills"),
+    path("skills/",views.skills,name="skills"),
+    path("skills",views.skills,name="skills"), 
+    path("projects/",views.projects,name="projects"),
     path("projects",views.projects,name="projects"),
     path("contact",views.contact_view,name="contact"),
     path("contact/",views.contact_view,name="contact"),
